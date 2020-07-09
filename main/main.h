60f60424b63a6fdbb48839ac88f35ca703dd0483
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-07-09 14:07:50 +0200

    Combined output file for the whole main module, together with appropriate header file to use it.

---
+++ main/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_MAIN_H
+#define _MAIN_MAIN_H 1
+
+/* Include all header files at once: */
+#include "apic.h"
+#include "cpu.h"
+#include "dma.h"
+#include "err.h"
+#include "intr.h"
+#include "keyboard.h"
+#include "kmalloc.h"
+#include "lterm.h"
+#include "main.h"
+#include "memory.h"
+#include "semaphore.h"
+#include "system.h"
+#include "time.h"
+
+
+#endif
